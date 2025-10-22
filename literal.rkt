4883c6e3b7d463b0f7b612311dbffa9b68bcd867
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-22 01:21:19 -0500

    literal.rkt
    Updated and changed the code. Now is working like it should.

--- literal.rkt
+++ literal.rkt
@@ -1,10 +1,14 @@
 #lang racket
-(provide eval-number eval-dollar eval-token-id)
-(require "ParseAndList.rkt" "history.rkt")
-
-(define (eval-token-id cs)
-  (parse-int/list (cdr cs)))
+(provide eval-number eval-dollar)
+(require "ParseAndList.rkt" "history.rkt" "whiteSpace.rkt")
 
 (define (eval-number cs hist)
-  (define p (parse-int/list cs))
-  (list (first p) (second p)))
+  (let ([p (parse-int/list cs)])
+    (list (first p) (second p))))
+
+(define (eval-dollar cs hist)
+  (let ([cs0 (skip-ws cs)])
+    (unless (and (pair? cs0) (char=? (car cs0) #\$))
+      (error 'parse "$ expected"))
+    (define p (parse-int/list (cdr cs0)))
+    (list (history-get-by-id hist (first p)) (second p))))
