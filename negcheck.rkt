6153925acd691cd509cf3cf661314edd61827e2a
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-22 15:48:02 -0500

    negcheck.rkt added more code

--- negcheck.rkt
+++ negcheck.rkt
@@ -2,3 +2,10 @@
 
 (provide starts-negative-literal? raw-minus?)
 (require "scanner.rkt")
+
+(define (raw-minus? ch) (char=? ch #\-))
+
+(define (starts-negative-literal? cs)
+  (and (pair? cs)
+       (raw-minus? (car cs))
+       (digit? (cadr cs))))
