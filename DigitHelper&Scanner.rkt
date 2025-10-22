c5bd4fe2e029e325ecb34d74f718b51e7a5643cd
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-21 20:32:01 -0500

    Delete DigitHelper&Scanner.rkt
    Dont need the file anymore since it has been replaced with Scanner.rkt

--- DigitHelper&Scanner.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(define (digit? ch) (and (char>=? ch #\0) (char<=? ch #\9)))
-(define (take-while pred cs)
-  (let loop ([xs cs] [acc '()])
-    (cond
-      [(and (pair? xs) (pred (car xs))) (loop (cdr xs) (cons (car xs) acc))]
-      [else (values (reverse acc) xs)])))
