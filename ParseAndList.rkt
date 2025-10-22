a9b6d89441b03d98288bb9821c8263a65a072c0f
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-21 20:13:02 -0500

    Updated/New version of ParseAndList

---
+++ ParseAndList.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(provide parse-int/list)
+
+(require "whiteSpace.rkt"      
+         "scanner.rkt")  
+(define (parse-int/list cs)
+  (let* ([cs0 (skip-ws cs)]
+         [neg? (and (pair? cs0) (char=? (car cs0) #\-))]
+         [after (if neg? (cdr cs0) cs0)])
+    (unless (and (pair? after) (digit? (car after)))
+      (error 'parse "expected integer"))
+    (define-values (digits rest) (take-while digit? after))
+    (define token (list->string (append (if neg? (list #\-) '()) digits)))
+    (define n (string->number token))
+    (unless (and n (integer? n)) (error 'parse "bad integer"))
+    (list (exact-round n) rest)))
