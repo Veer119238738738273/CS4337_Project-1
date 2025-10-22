793efda1469b25754d22ac05fa2aa1247ee9d16f
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-22 18:47:03 -0500

    eval_core.rkt updated code

--- eval_core.rkt
+++ eval_core.rkt
@@ -10,3 +10,13 @@
 (define (eval-un op cs hist eval-expr)
   (define e1 (eval-expr cs hist))
   (list (apply-un op (first e1)) (second e1)))
+
+(define (eval-expr cs hist)
+  (cond
+    [(char=? (car cs) #\+) (eval-bin '+ (cdr cs) hist eval-expr)]
+    [(char=? (car cs) #\*) (eval-bin '* (cdr cs) hist eval-expr)]
+    [(char=? (car cs) #\/) (eval-bin '/ (cdr cs) hist eval-expr)]
+    [(char=? (car cs) #\-) (eval-un '- (cdr cs) hist eval-expr)] 
+    [(char=? (car cs) #\$) (eval-dollar cs hist)]
+    [(digit? (car cs)) (eval-number cs hist)]
+    [else (error 'parse "invalid token")]))
