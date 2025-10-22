aa5656dacaa8f76765ef91f71dbced60c828ecef
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-22 16:29:08 -0500

    function eval_core.rkt
    started working on new function

---
+++ eval_core.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(provide eval-expr eval-bin eval-un)
+(require "arith.rkt" "literal.rkt" "whiteSpace.rkt" "negcheck.rkt" "scanner.rkt")
+
+(define (eval-bin op cs hist eval-expr)
+  (define e1 (eval-expr cs hist))
+  (define e2 (eval-expr cs hist))
+  (list (apply-bin op (first e1) (first e2)) (second e2)))
+
