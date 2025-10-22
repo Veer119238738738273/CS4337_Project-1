fc3fb6641a09ffb9bdfa13a709dfd5ee2d06aeb3
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-21 23:46:53 -0500

    corrected error for arith.rkt

--- arith.rkt
+++ arith.rkt
@@ -1,15 +1,14 @@
 #lang racket
-(provide apply-bin apply-un debug-op?)
-
-(define (debug-op? sym)
-  (member sym '(+ * / -)))
+(provide apply-bin apply-un)
 
 (define (apply-bin op a b)
-  (cond [(eq? op '+) (+ a b)]
-        [(eq? op '*) (* a b)]
-        [(eq? op '/) (/ a b)]
+  (cond [(equal? op '+) (+ a b)]
+        [(equal? op '*) (* a b)]
+        [(equal? op '/) (begin
+                          (when (zero? b) (error 'eval "divide by zero"))
+                          (quotient a b))]
         [else (error 'apply-bin "unknown op")]))
 
 (define (apply-un op a)
-  (cond [(eq? op '-) (- a)]
+  (cond [(equal? op '-) (- a)]
         [else (error 'apply-un "unknown unary op")]))
