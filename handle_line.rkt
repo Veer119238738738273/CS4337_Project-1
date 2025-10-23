bf094f944e6d85c4ee59a41934e78d681cfaabba
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-23 13:02:50 -0500

    completed handle_line.rkt function
    I think I fixed all errors and it seems to be running correctly

--- handle_line.rkt
+++ handle_line.rkt
@@ -1,13 +1,15 @@
 #lang racket
 (provide handle-line)
-(require "line_eval.rkt" "io_fmt.rkt" "history.rkt")
+(require "line_eval.rkt"
+         "io_fmt.rkt"
+         "history.rkt")
 
 (define (handle-line s hist)
   (with-handlers ([exn:fail? (λ (e)
                                (print-error)
-                               (cons 0 hist))])   
+                               hist)])
     (define out (evaluate-line s hist))
     (define value (first out))
-    (define id (result-id (cons value hist)))
+    (define id (result-id hist))     
     (print-result id value)
-    (cons value hist)))
+    (cons value hist)))                 
