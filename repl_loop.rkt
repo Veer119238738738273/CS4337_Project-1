1d984a00ce572ebb6a1aa8302fd5667b48ef197d
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-23 16:08:33 -0500

    finished the repl_loop funtion
    Now whenever an operation is done it will increment the counter now instead of just staying 1:
    
    Example:
    Before:
    1: 3.0
    > + 1 2
    1: 3.0
    > + 1 2
    1: 3.0
    > + 1 2
    
    After:
    1: 3.0
    > + 1 2
    2: 3.0
    > + 1 2
    3: 3.0
    > + 1 2

--- repl_loop.rkt
+++ repl_loop.rkt
@@ -5,3 +5,11 @@
 (define (maybe-prompt)
   (when interactive?
     (display "> ") (flush-output)))
+
+(define (repl hist)
+  (maybe-prompt)
+  (define line (read-line))
+  (cond
+    [(eof-object? line) (void)]
+    [(string=? line "quit") (void)]
+    [else (repl (handle-line line hist))]))
