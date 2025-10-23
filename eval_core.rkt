db4780ebf78e35623480cf2c06179e4af1822e25
Veer119238738738273 <47932972+Veer119238738738273@users.noreply.github.com>
2025-10-22 22:16:43 -0500

    Finished working on my eval_core.rkt function
    I ran into several errors during the implementation. Most came from how I handled whitespace and negative numbers. I updated ParseAndList.rkt so inputs with a space between the minus sign and the digits no longer break the parser. Now the function treats “- 5” as a single negative literal where intended and passes clean tokens to the evaluator.

--- eval_core.rkt
+++ eval_core.rkt
@@ -1,22 +1,39 @@
 #lang racket
-(provide eval-expr eval-bin eval-un)
-(require "arith.rkt" "literal.rkt" "whiteSpace.rkt" "negcheck.rkt" "scanner.rkt")
+(provide eval-expr)
 
-(define (eval-bin op cs hist eval-expr)
-  (define e1 (eval-expr cs hist))
-  (define e2 (eval-expr cs hist))
-  (list (apply-bin op (first e1) (first e2)) (second e2)))
+(require "arith.rkt"       
+         "literal.rkt"     
+         "whiteSpace.rkt" 
+         "negcheck.rkt"    
+         "scanner.rkt")    
 
-(define (eval-un op cs hist eval-expr)
+(define (eval-bin op cs hist)
   (define e1 (eval-expr cs hist))
-  (list (apply-un op (first e1)) (second e1)))
+  (define v1 (first e1))
+  (define r1 (second e1))
+  (define e2 (eval-expr r1 hist))
+  (define v2 (first e2))
+  (define r2 (second e2))
+  (list (apply-bin op v1 v2) r2))
+
+(define (eval-un op cs hist)
+  (define e1 (eval-expr cs hist))
+  (define v1 (first e1))
+  (define r1 (second e1))
+  (list (apply-un op v1) r1))
 
 (define (eval-expr cs hist)
-  (cond
-    [(char=? (car cs) #\+) (eval-bin '+ (cdr cs) hist eval-expr)]
-    [(char=? (car cs) #\*) (eval-bin '* (cdr cs) hist eval-expr)]
-    [(char=? (car cs) #\/) (eval-bin '/ (cdr cs) hist eval-expr)]
-    [(char=? (car cs) #\-) (eval-un '- (cdr cs) hist eval-expr)] 
-    [(char=? (car cs) #\$) (eval-dollar cs hist)]
-    [(digit? (car cs)) (eval-number cs hist)]
-    [else (error 'parse "invalid token")]))
+  (let* ([cs0 (skip-ws cs)])
+    (if (null? cs0)
+        (error 'parse "unexpected end of input")
+        (let ([ch (car cs0)])
+          (cond
+            [(char=? ch #\+) (eval-bin '+ (cdr cs0) hist)]
+            [(char=? ch #\*) (eval-bin '* (cdr cs0) hist)]
+            [(char=? ch #\/) (eval-bin '/ (cdr cs0) hist)]
+            [(char=? ch #\-) (if (starts-negative-literal? cs)
+                                 (eval-number cs hist)           
+                                 (eval-un '- (cdr cs0) hist))]   
+            [(char=? ch #\$) (eval-dollar cs hist)]              
+            [(digit? ch) (eval-number cs hist)]                 
+            [else (error 'parse "invalid token")])))))
